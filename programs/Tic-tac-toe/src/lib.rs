a4729246043878a60510249fd902fdb672aca26f
Kim Jae Hoon <hoon.development@gmail.com>
2022-07-11 17:53:35 +0900

    chore: 프로그램 id 수정

--- programs/Tic-tac-toe/src/lib.rs
+++ programs/Tic-tac-toe/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("2Vp6DmE6YL9ffS8bb215YbWS6jBBWtADpmpXFYbx2eRv");
 
 #[program]
 pub mod tic_tac_toe {
