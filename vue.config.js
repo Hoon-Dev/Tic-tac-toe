ae71a0cf85f04f2ac1dad619e27be9cafc9a0751
Kim Jae Hoon <hoon.development@gmail.com>
2022-07-11 19:00:02 +0900

    fix: web3 폴리필 무시하도록 설정

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    resolve: {
+      fallback: {
+        "crypto": false,
+        "stream": false
+      }
+    }
+  }
 })
