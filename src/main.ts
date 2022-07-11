4e3b64e26d32a3057ae51afb04a4a4d86d84d2c9
Kim Jae Hoon <hoon.development@gmail.com>
2022-07-11 18:59:48 +0900

    feat: 솔라나 지갑 어뎁터 추가

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,27 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-createApp(App).use(store).use(router).mount('#app')
+import SolanaWallets from "solana-wallets-vue";
+import "solana-wallets-vue/styles.css";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+
+import {
+  PhantomWalletAdapter,
+  SlopeWalletAdapter,
+  SolflareWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+
+const walletOptions = {
+  wallets: [
+    new PhantomWalletAdapter(),
+    new SlopeWalletAdapter(),
+    new SolflareWalletAdapter({ network: WalletAdapterNetwork.Devnet })
+  ],
+  autoConnect: true,
+};
+
+createApp(App).use(store).use(router).use(SolanaWallets, walletOptions).mount("#app");
+
+
